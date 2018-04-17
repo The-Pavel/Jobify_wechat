0375b0bb56d3e77de97a2f73dc8f8696cb36d315
Wendy Lee <wanderlustwendy@icloud.com>
2018-04-17 19:06:46 +0800

    modification on nav bar and index page link

--- pages/index/index.js
+++ pages/index/index.js
@@ -7,11 +7,11 @@ Page({
     userInfo: {}
   },
   //事件处理函数
-  bindViewTap: function () {
-    wx.navigateTo({
-      url: '../logs/logs'
-    })
-  },
+  // bindViewTap: function () {
+  //   wx.navigateTo({
+  //     url: '../logs/logs'
+  //   })
+  // },
 
   postjob: function (e) {
     wx.navigateTo({
@@ -31,15 +31,21 @@ Page({
     })
   },
 
-  onLoad: function () {
-    console.log('onLoad')
-    var that = this
-    //调用应用实例的方法获取全局数据
-    app.getUserInfo(function (userInfo) {
-      //更新数据
-      that.setData({
-        userInfo: userInfo
-      })
+  takequiz: function (e) {
+    wx.navigateTo({
+      url: `/pages/quiz/quiz`,
     })
-  }
+  },
+
+  // onLoad: function () {
+  //   console.log('onLoad')
+  //   var that = this
+  //   //调用应用实例的方法获取全局数据
+  //   app.getUserInfo(function (userInfo) {
+  //     //更新数据
+  //     that.setData({
+  //       userInfo: userInfo
+  //     })
+  //   })
+  // }
 })
