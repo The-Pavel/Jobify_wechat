b3a636ffe3f06950162203c8fa4d7a7459f288a4
Isabel Chan <isabelchan1994@gmail.com>
2018-04-19 10:19:10 +0800

    edit page in progress

--- pages/postedjobs/postedjobs.js
+++ pages/postedjobs/postedjobs.js
@@ -1,66 +1,109 @@
-// pages/postedjobs/postedjobs.js
+var app = getApp()
 Page({
-
-  /**
-   * 页面的初始数据
-   */
   data: {
-  
+    saved_jobs: [],
+    items: [],
+    startX: 0, //开始坐标
+    startY: 0
   },
+  onLoad: function () {
+    const page = this
+    const user = wx.getStorageSync('user')
+    let data = { id: user.id }
+    wx.request({
+      url: 'http://localhost:3000/api/v1/jobs/',
+      method: 'PUT',
+      data: data,
+      success: function (res) {
+        page.setData({ saved_jobs: res.data })
 
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function (options) {
-  
+        for (var i = 0; i < page.data.saved_jobs.length; i++) {
+          page.data.items.push({
+            job_title: page.data.saved_jobs[i].title,
+            company_name: page.data.saved_jobs[i].company,
+            image: page.data.saved_jobs[i].image,
+            isTouchMove: false //默认全隐藏删除
+          })
+        }
+        page.setData({
+          items: page.data.items
+        })
+      },
+
+
+    })
   },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-  
+  touchstart: function (e) {
+    const page = this
+    //开始触摸时 重置所有删除
+    page.data.items.forEach(function (v, i) {
+      if (v.isTouchMove)//只操作为true的
+        v.isTouchMove = false;
+    })
+    page.setData({
+      startX: e.changedTouches[0].clientX,
+      startY: e.changedTouches[0].clientY,
+      items: page.data.items
+    })
   },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-  
+  //滑动事件处理
+  touchmove: function (e) {
+    var that = this,
+      index = e.currentTarget.dataset.index,//当前索引
+      startX = that.data.startX,//开始X坐标
+      startY = that.data.startY,//开始Y坐标
+      touchMoveX = e.changedTouches[0].clientX,//滑动变化坐标
+      touchMoveY = e.changedTouches[0].clientY,//滑动变化坐标
+      //获取滑动角度
+      angle = that.angle({ X: startX, Y: startY }, { X: touchMoveX, Y: touchMoveY });
+    that.data.items.forEach(function (v, i) {
+      v.isTouchMove = false
+      //滑动超过30度角 return
+      if (Math.abs(angle) > 30) return;
+      if (i == index) {
+        if (touchMoveX > startX) //右滑
+          v.isTouchMove = false
+        else //左滑
+          v.isTouchMove = true
+      }
+    })
+    //更新数据
+    that.setData({
+      items: that.data.items
+    })
   },
-
   /**
-   * 生命周期函数--监听页面隐藏
+   * 计算滑动角度
+   * @param {Object} start 起点坐标
+   * @param {Object} end 终点坐标
    */
-  onHide: function () {
-  
+  angle: function (start, end) {
+    var _X = end.X - start.X,
+      _Y = end.Y - start.Y
+    //返回角度 /Math.atan()返回数字的反正切值
+    return 360 * Math.atan(_Y / _X) / (2 * Math.PI);
   },
+  //edit事件
+ edit: function (e) {
+    const page = this
+    const user = wx.getStorageSync('user')
+    const index = e.currentTarget.dataset.index
+    page.data.items.splice(e.currentTarget.dataset.index, 1)
+    page.setData({
+      items: page.data.items
+    })
 
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-  
-  },
+    const data = { user_id: user.id, i: index }
+    wx.request({
+      url: `http://localhost:3000/api/v1/users/${user.id}/saved_jobs`,
+      method: 'PUT',
+      data: data,
+      success: function (res) {
+        console.log(res)
+      }
 
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-  
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-  
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-  
+    })
   }
-})
+
+
+}, )
