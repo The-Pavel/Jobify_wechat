fcc0236d231ae208df35bf5570b8a24a139d7db1
Wendy Lee <wanderlustwendy@icloud.com>
2018-04-19 23:52:42 +0800

    updated links on index page

--- app.js
+++ app.js
@@ -19,7 +19,7 @@ App({
             success: res => {
               console.log(res.userInfo)
               // 可以将 res 发送给后台解码出 unionId
-              app.globalData.userInfo = res.userInfo
+              app.setData({ userInfo: res.userInfo})
             }
           })
 
